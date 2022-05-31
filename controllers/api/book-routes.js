2b1d66b0f65c08efc1296097f5ce7ad630611786
Jemima Siddiqui <jemimasiddiqui1@gmail.com>
2022-05-31 20:57:58 +1000

    edited book-routes and index files in controller to remove a minor bug

--- controllers/api/book-routes.js
+++ controllers/api/book-routes.js
@@ -124,7 +124,7 @@ router.delete("/:id", withAuth, (req, res) => {
             },
         })
         .then((dbCommentData) => {
-            if (!dbPostData) {
+            if (!dbCommentData) {
                 res.status(404).json({
                     message: "No comment found with this id"
                 });
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
-const postRoutes = require('./book-routes.js');
+const bookRoutes = require('./book-routes.js');
 //const commentRoutes = require('./comment-routes.js');
 
 router.use('/users', userRoutes);
-router.use('/books', postRoutes);
+router.use('/books', bookRoutes);
 //router.use('/comments', commentRoutes)
 
 module.exports = router;
