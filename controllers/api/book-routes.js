86dee42f9303ea798ba514ca12745a9e3738a7f5
Jazzberries <anime.flick.chick@gmail.com>
2022-06-02 12:31:38 +1000

    Added minimal styling to other handlebars pages for consistency across site

--- controllers/api/book-routes.js
+++ controllers/api/book-routes.js
@@ -35,7 +35,7 @@ router.get("/", (req, res) => {
 
             res.render('library', {
                 book,
-                loggedIn: req.session.loggedIn
+                loggedIn: req.session.loggedIn,
             });
         })
         // res.send('GET request for the book page'); 
